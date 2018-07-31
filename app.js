3f75a900c8803ad5a42d85bb149662dd932007f8
Brecht Vermeire <brecht@Brechts-MacBook-Pro.local>
2018-07-31 18:05:29 +0100

    stuff

--- app.js
+++ app.js
@@ -17,23 +17,19 @@ $(function() {
                         lat: lat,
                         lng: lng
                     };
-                    // initialize(returned_location);
-                    initialize({
-                        lat: 51.208785,
-                        lng: 3.224299
-                    })
+                    initialize(returned_location);
+                    // initialize({
+                    //     lat: 51.208785,
+                    //     lng: 3.224299
+                    // })
                 },
                 function(error) {
-                    initialize({
-                        lat: 51.208785,
-                        lng: 3.224299
-                    });
+                    $("#map_placeholder").html("Location not found!")
+                    update_card_text("Uh oh.", "Location not found!", "Please enable location services to use this app.")
                 });
         } else {
-            initialize({
-                lat: 51.208785,
-                lng: 3.224299
-            });
+            $("#map_placeholder").html("Location not found!")
+            update_card_text("Uh oh.", "Location not found!", "Please enable location services to use this app.")
         }
     }
 
@@ -45,7 +41,16 @@ function initialize(location) {
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: current_location,
-        zoom: 15
+        styles: mapstyles,
+        zoom: 15,
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: false,
+        gestureHandling: 'greedy'
+
     });
 
     var current_location_marker = new google.maps.Marker({
@@ -57,22 +62,27 @@ function initialize(location) {
     markers.push(current_location_marker)
 
     var request = {
-        query: "bars or pubs",
         location: current_location,
-        radius: '1000',
+        radius: '500',
         openNow: true,
-        types: ['cafe']
+        types: ['pub']
     };
 
     service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, callback);
+    // service.textSearch(request, callback);
+    service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
             var place = results[i];
-            createMarker(place)
+            if (
+                place.types.indexOf("pub") > -1 ||
+                place.types.indexOf("bar") > -1
+            ) {
+                createMarker(place)
+            }
         }
     }
 }
@@ -91,7 +101,7 @@ function createMarker(place) {
 
     var marker = new google.maps.Marker({
         position: place.geometry.location,
-        icon: dot,
+        icon: "images/beer_icon_32.png",
         map: map,
         title: "marker"
     });
@@ -101,7 +111,6 @@ function createMarker(place) {
     marker.addListener('click', function() {
         directionsDisplay.setMap(null);
         directionsDisplay.setMap(map);
-        console.log(place)
         card_clickhandler(
             directionsService,
             directionsDisplay,
@@ -130,24 +139,33 @@ function card_clickhandler(directionsService, directionsDisplay, place, pointA, 
 
             duration = response.routes[0].legs[0].duration.text
             distance = response.routes[0].legs[0].distance.text
-            $("#card_subtitle").html('<span style="color: #353535; font-style: bold;">' + duration + " walking (" + distance + ")</span>")
+            distance_duration = '<span style="color: #353535; font-style: bold;">' + duration + " walking (" + distance + ")</span>"
 
-        } else {
-            // window.alert('Directions request failed due to ' + status);
+            var name = place.name
+            var address = place.vicinity
+            var rating = place.rating
+            var rating_color;
+            if (rating >= 4) {
+                rating_color = "#60BD68"
+            } else if (rating >= 3) {
+                rating_color = "#FAA43A"
+            } else {
+                rating_color = "#F15854"
+            }
+            var open = ("Open Now" ? place.opening_hours != undefined && place.opening_hours.open_now : "Closed")
+            title_html = name
+            address_html = distance_duration + "<br>" + '<span style="color: #656565;">' + address + "</span>"
+
+            rating_html = '<span id="card_rating_span" style="background-color: ' + rating_color + ';">' + rating + " / 5</span>"
+            update_card_text(title_html, address_html, rating_html)
         }
 
-        var name = place.name
-        var address = place.formatted_address
-        var rating = place.rating
-        var open = ("Open Now" ? place.opening_hours != undefined && place.opening_hours.open_now : "Closed")
-        $("#card_title").html(name)
-        $("#card_subtitle").append("<br>" + '<span style="color: #656565;">' + address + "</span>")
-        $("#card_rating").html(rating + " / 5")
+
     });
 }
 
 function update_card_text(title, content, rating) {
     $("#card_title").html(title)
-    $("#card_subtitle").append(content)
+    $("#card_subtitle").html(content)
     $("#card_rating").html(rating)
 }
--- appold.js
+++ appold.js
@@ -243,11 +243,3 @@ function clear() {
     $(".clear_content").remove()
 }
 
-$.fn.stars = function() {
-    return $(this).each(function() {
-        var val = parseFloat($(this).html());
-        var size = Math.max(0, (Math.min(5, val))) * 16;
-        var $span = $('<span />').width(size);
-        $(this).html($span);
-    });
-}
